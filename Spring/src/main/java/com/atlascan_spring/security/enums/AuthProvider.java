0ab3612b5280c48db5ea72c7bdb3a5fdd7bce5d4
BilalMO <azbilal.motassim@gmail.com>
2024-12-15 00:29:09 +0100

    Initialize submodules

--- Spring/src/main/java/com/atlascan_spring/security/enums/AuthProvider.java
+++
@@ -1,6 +0,0 @@
-package com.atlascan_spring.security.enums;
-
-public enum AuthProvider {
-    LOCAL,
-    GOOGLE
-}
--- Spring/src/main/java/com/atlascan_spring/security/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.atlascan_spring.security.enums;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    OIDC_ROLE
-}
